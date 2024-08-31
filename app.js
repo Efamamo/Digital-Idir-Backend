674b7737fb349290e695e50a813f88a9d1f96018
ephrem <ephremmamo555@gmail.com>
2024-08-31 12:30:48 +0300

    add auth routes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 
+const authRouter = require("./routes/auth")
+
 const app = express()
+app.use(cors())
 
 app.get("/", (req,res)=>{
     res.send("Hello World")
 })
+
+app.use("/api/v1/auth", authRouter)
 
 
 app.listen(5000)
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router();
+
+router.post("/signup")
+router.post("/login")
+router.post("/logout")
+
+
+
+module.exports = router
