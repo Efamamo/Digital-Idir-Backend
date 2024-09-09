f3e3e43c565e4523e3fc6b22b76801b4525b3d1a
ephrem <ephremmamo555@gmail.com>
2024-09-09 14:57:01 +0300

    update google passport

--- services/passport-setup.js
+++ services/passport-setup.js
@@ -1,15 +1,15 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
-const User = require("../models/user");
-const Refresh = require("../models/refresh");
-const jwtService = require("./jwt-service");
-require("dotenv").config();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const User = require('../models/user');
+const Refresh = require('../models/refresh');
+const jwtService = require('./jwt-service');
+require('dotenv').config();
 
 passport.use(
   new GoogleStrategy(
     {
       //options for staratagy
-      callbackURL: "api/v1/auth/callback",
+      callbackURL: 'api/v1/auth/callback',
       clientID: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     },
@@ -18,7 +18,7 @@ passport.use(
       const emailVerified = profile.emails[0].verified;
 
       if (!emailVerified) {
-        return done(null, false, { message: "Email not verified" });
+        return done(null, false, { message: 'Email not verified' });
       }
 
       try {
@@ -44,7 +44,20 @@ passport.use(
           });
 
           const user = await newUser.save();
-          done(null, { user: user });
+          const token = jwtService.generateToken(user);
+          const refreshToken = jwtService.generateRefreshToken(user);
+
+          const newRefresh = new Refresh({
+            token: refreshToken,
+          });
+
+          await newRefresh.save();
+
+          done(null, {
+            user,
+            accessToken: token,
+            refreshToken: refreshToken,
+          });
         }
       } catch (e) {
         done(e, null);
